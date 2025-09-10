c9f5792dbecf6540342f287b3d9e763d62ebfd24
Ana <anapazparietti@gmail.com>
2025-09-10 09:36:05 -0300

    clalendario hover

--- pruebasAna/scripts/script-calendario.js
+++ pruebasAna/scripts/script-calendario.js
@@ -1,12 +1,21 @@
 document.addEventListener ('DOMContentLoaded', () => {
-    const lineaTiempo = [
-        {año: '1930/40', dato: 'Raymond Scott, pionero en la música electrónica y experimental.'},
-        {año: '1950', dato: 'Illiac Suite: primera pieza generada por computadora. Raymond Scott desarrolla el Electronium, máquina que genera música a partir de patrones aleatorios.'},
-        {año: '1960/70', dato: 'MUSIC-N: programas pioneros de síntesis digital (Max Mathews). Primer canto sintético en computadora (“Daisy Bell”), inspiración para HAL 9000.'}
-    ]
+     // 1. Obtener los elementos del HTML
+        const mensajes = document.querySelectorAll('.mensaje-oculto');
+        const evento = document.querySelectorAll('.evento img');
 
-   
-console.log(lineaTiempo[0]);  
+        // 2. Escuchar cuando el ratón entra (mouseover)
+        //se hace un forEach para recorrer cada imagen y comparar si su index (recordar que el querySelectorAll devuelve un NodeList, que es un tipo de array) es el que coincide con el mensaje que debe visualizarse
+        evento.forEach((img, index) => {
+            img.addEventListener('mouseover', () => {
+                mensajes[index].classList.add('visible'); // Añade la clase 'visible'
+            });
+        });
 
+        // 4. Escuchar cuando el ratón sale (mouseout)
+        evento.forEach((img, index) => {
+            img.addEventListener('mouseout', () => {
+                mensajes[index].classList.remove('visible'); // Quita la clase 'visible'
+            });
+        });
 
 });
