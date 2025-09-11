7c68cfe0db856692cca9fa2f181bebb6704d17aa
Ana <anapazparietti@gmail.com>
2025-09-11 16:17:40 -0300

    script folder listo

--- pruebasAna/scripts/script-folder.js
+++ pruebasAna/scripts/script-folder.js
@@ -1,23 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     const fotoItems = document.querySelectorAll(".foto-item");
     const fotoOculta = document.querySelector(".foto-alumnos-escondida");
-
+    let esVisible = false;
+    const target = 0;// index del array de fotos
+    
     fotoItems.forEach((img, i) => {
             img.addEventListener('click', () => {
-                fotoOculta.classList.add('visible'); // Añade la clase 'visible'
+                if(!esVisible){
+                    fotoOculta.classList.add('visible'); // Añade la clase 'visible'
+                    esVisible = true;
+                    target = img[i];
+                }
             });
         });
-    fotoOculta.addEventListener('mouseleave',()=>{
-        
+    fotoOculta.addEventListener('mouseout',()=>{
+        if (esVisible) {
+            fotoOculta.classList.remove('visible');
+            esVisible = false;
+        }
     })
-
+/* RENDERIZADO DE FOTOS */
     const fotoAlumnos = [
-        {foto: 'https://placehold.co/300x400'},
-        {foto: 'https://placehold.co/300x400'},
-        {foto: 'https://placehold.co/300x400'},
-        {foto: 'https://placehold.co/300x400'},
-        {foto: 'https://placehold.co/300x400'},
-        {foto: 'https://placehold.co/300x400'},
-        {foto: 'https://placehold.co/300x400'}
+        {foto: 'https://placehold.co/300x401'},
+        {foto: 'https://placehold.co/300x402'},
+        {foto: 'https://placehold.co/300x401'},
+        {foto: 'https://placehold.co/300x403'},
+        {foto: 'https://placehold.co/300x404'},
+        {foto: 'https://placehold.co/300x405'},
+        {foto: 'https://placehold.co/300x406'}
     ]
+
+function renderFotos(){
+
+}
+
+    if(fotoOculta){
+        fotoOculta.innerHTML = `
+            <img src="${fotoAlumnos[target].foto}" alt="Foto alumno">`
+
+    }
 });
